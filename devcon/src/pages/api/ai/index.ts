caed0dff17b24dd12de4ca3a0a7de2149d6b6ba4
lassejaco <lasse.jacobsen@ethereum.org>
2024-05-09 15:35:21 +0200

    holy ai

---
+++ devcon/src/pages/api/ai/index.ts
@@ -0,0 +1,23 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { api } from './open-ai'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  console.log(req.method, 'method')
+  console.log(req.query, 'query')
+  console.log(req.body, 'body')
+  //   const { threadID } = req.query
+
+  if (req.method === 'GET') {
+    return res.send('get request hello')
+  } else if (req.method === 'POST') {
+    const { message, threadID } = JSON.parse(req.body)
+
+    console.log(message, threadID, 'msg thread id')
+
+    const result = await api.createMessage('asst_sWNkGoBZViwje5VdkLU46oZV', message, threadID)
+
+    return res.json(result)
+  }
+
+  return res.send('hello')
+}
