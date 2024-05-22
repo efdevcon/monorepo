60199910b19d873a026b8919a1f88539b3c7d57c
wslyvh <wslyvh@users.noreply.github.com>
2024-05-22 15:06:32 +0200

    cors

--- devcon-api/src/app.ts
+++ devcon-api/src/app.ts
@@ -22,8 +22,15 @@ app.use(logHandler)
 
 const corsConfig: cors.CorsOptions = {}
 if (SERVER_CONFIG.NODE_ENV === 'production') {
-  corsConfig.origin = '*', // allow all origins
-  corsConfig.credentials = true
+  ;(corsConfig.origin = [
+    '*',
+    'https://devcon.org/',
+    'https://www.devcon.org/',
+    'https://app.devcon.org/',
+    'https://archive.devcon.org/',
+    'https://live.devcon.org/',
+  ]),
+    (corsConfig.credentials = true)
 }
 app.use(cors(corsConfig))
 
