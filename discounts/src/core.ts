74f7344cabfc51c1401a7c0b794f0365c929ba36
wslyvh <wslyvh@users.noreply.github.com>
2024-06-25 16:25:20 +0200

    discounts

---
+++ discounts/src/core.ts
@@ -0,0 +1,13 @@
+import fs from 'fs'
+
+parse()
+
+async function parse() {
+    const merge = JSON.parse(fs.readFileSync('inputs/the-merge-contributors.json', 'utf-8'))
+    const pg = JSON.parse(fs.readFileSync('inputs/protocol-guild.json', 'utf-8'))
+
+    const uniques = Array.from(new Set([...merge, ...pg]))
+
+    console.log('Merge Pass', merge.length, 'Protocol Guild', pg.length, 'Unique', uniques.length)
+    fs.writeFileSync('outputs/core-devs.json', JSON.stringify(uniques, null, 2), 'utf-8');
+}
