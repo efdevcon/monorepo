0b9c814c699b9739e0c8446f7abc55c1cdbb4875
wslyvh <wslyvh@users.noreply.github.com>
2024-07-08 16:58:49 +0200

    disable claim db

--- devcon/src/pages/api/discounts/claim/[id].ts
+++ devcon/src/pages/api/discounts/claim/[id].ts
@@ -1,10 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { getServerSession } from 'next-auth'
 import { authOptions } from 'pages/api/auth/[...nextauth]'
-import { PrismaClient } from '@prisma/client'
 import { getVoucherCodes } from 'utils/vouchers'
-
-const client = new PrismaClient()
+// import { PrismaClient } from '@prisma/client'
+// const client = new PrismaClient()
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const id = req.query.id as string
@@ -14,20 +13,20 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if (!session) return res.status(401).json({ error: 'Unauthorized' })
 
     const voucher = await getVoucherCodes('core-devs', 3)
-    await client.discountClaims.upsert({
-        where: {
-            id: `${id}_${voucher}`,
-        },
-        update: {
-            identifier: id,
-            voucher: voucher,
-        },
-        create: {
-            id: `${id}_${voucher}`,
-            identifier: id,
-            voucher: voucher,
-        }
-    })
+    // await client.discountClaims.upsert({
+    //     where: {
+    //         id: `${id}_${voucher}`,
+    //     },
+    //     update: {
+    //         identifier: id,
+    //         voucher: voucher,
+    //     },
+    //     create: {
+    //         id: `${id}_${voucher}`,
+    //         identifier: id,
+    //         voucher: voucher,
+    //     }
+    // })
     
     res.status(200).json({ data: {
         id,
