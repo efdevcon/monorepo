5401ad23dd928be359a3c33033493cffaa3374bd
wslyvh <wslyvh@users.noreply.github.com>
2024-07-16 17:42:24 +0200

    additional discounts

--- discounts/src/core.ts
+++ discounts/src/core.ts
@@ -5,9 +5,10 @@ parse()
 async function parse() {
     const merge = JSON.parse(fs.readFileSync('inputs/the-merge-contributors.json', 'utf-8'))
     const pg = JSON.parse(fs.readFileSync('inputs/protocol-guild.json', 'utf-8'))
+    const pg2 = JSON.parse(fs.readFileSync('inputs/pg2.json', 'utf-8'))
+    
+    const uniques = Array.from(new Set([...merge, ...pg, ...pg2]))
 
-    const uniques = Array.from(new Set([...merge, ...pg]))
-
-    console.log('Merge Pass', merge.length, 'Protocol Guild', pg.length, 'Unique', uniques.length)
+    console.log('Merge Pass', merge.length, 'Protocol Guild', pg.length, 'Protocol Guild #2', pg2.length, 'Unique', uniques.length)
     fs.writeFileSync('outputs/core-devs.json', JSON.stringify(uniques, null, 2), 'utf-8');
 }
