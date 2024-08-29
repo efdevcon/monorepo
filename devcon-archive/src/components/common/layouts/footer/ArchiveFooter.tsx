f6580be7aa5f3f5ab16d2f831d744983625aec16
wslyvh <wslyvh@users.noreply.github.com>
2024-08-29 18:25:38 +0200

    blog link

--- devcon-archive/src/components/common/layouts/footer/ArchiveFooter.tsx
+++ devcon-archive/src/components/common/layouts/footer/ArchiveFooter.tsx
@@ -43,7 +43,7 @@ export const ArchiveFooter = () => {
         },
         {
           title: 'Blog',
-          url: 'https://blog.ethereum.org/category/events/',
+          url: 'https://devcon.org/blogs/',
           type: 'link',
         },
       ],
