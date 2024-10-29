4040b4501e698ca72f8adbc658927abd8b36fb72
wslyvh <wslyvh@users.noreply.github.com>
2024-10-29 23:33:54 +0100

    wallet connections

---
+++ devcon-app/src/components/wallet.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { Button } from 'lib/components/button'
+import { useAppKit } from '@reown/appkit/react'
+
+export function TestWalletConnections() {
+  const { open } = useAppKit()
+  return (
+    <>
+      <p>
+        <w3m-button />
+      </p>
+      <p>
+        <Button
+          fat
+          fill
+          className="w-full plain mt-4"
+          color="purple-2"
+          onClick={(e: any) => {
+            e.preventDefault()
+            // Add a small delay to avoid cross-origin issues
+            setTimeout(() => {
+              open()
+            }, 0)
+          }}
+        >
+          Continue With Ethereum
+        </Button>
+      </p>
+    </>
+  )
+}
