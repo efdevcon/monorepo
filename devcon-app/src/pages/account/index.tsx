cfe4c1e2250688d7f29d9f27b2440478b44b5d6d
wslyvh <wslyvh@users.noreply.github.com>
2024-11-01 11:23:19 +0100

    pages

--- devcon-app/src/pages/account/index.tsx
+++ devcon-app/src/pages/account/index.tsx
@@ -1,11 +1,13 @@
 import { PrivatePage } from 'components/domain/app/account/PrivatePage'
 import SettingsPage from 'components/domain/app/account/Settings'
 import { AppLayout } from 'components/domain/app/Layout'
+import { SEO } from 'components/domain/seo'
 import React from 'react'
 
 export default (props: any) => {
   return (
     <AppLayout pageTitle="Account" breadcrumbs={[{ label: 'Account' }]}>
+      <SEO title="Account" />
       <PrivatePage>
         <SettingsPage {...props} />
       </PrivatePage>
--- devcon-app/src/pages/account/profile.tsx
+++ devcon-app/src/pages/account/profile.tsx
@@ -1,11 +1,13 @@
 import { PrivatePage } from 'components/domain/app/account/PrivatePage'
 import ProfileSettings from 'components/domain/app/account/settings/Profile'
 import { AppLayout } from 'components/domain/app/Layout'
+import { SEO } from 'components/domain/seo'
 import React from 'react'
 
 export default (props: any) => {
   return (
     <AppLayout pageTitle="Profile Settings" breadcrumbs={[{ label: 'Profile Settings' }]}>
+      <SEO title="Account" />
       <PrivatePage>
         <ProfileSettings {...props} />
       </PrivatePage>
