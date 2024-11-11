52754ae92f3faaa53206b987f559716556fc9c29
lassejaco <lassejacobsen@ethereum.org>
2024-11-12 02:59:24 +0700

    remove mock

--- devcon-app/src/context/app-context.tsx
+++ devcon-app/src/context/app-context.tsx
@@ -21,13 +21,13 @@ export const AppContext = (props: AppContextProps) => {
   // Sync current time periodically to keep time related functionality up to date
   useEffect(() => {
     // const mockedTime = moment.utc('2024-10-23T19:00:00Z').utcOffset(7)
-    const mockedTime = moment.utc('2024-11-14T05:39:00Z').add(7, 'hours') // First day devcon
+    // const mockedTime = moment.utc('2024-11-14T05:39:00Z').add(7, 'hours') // First day devcon
     // // const mockedTime = moment.utc('2024-11-13T09:30:00Z') // Second day devcon
     // // const mockedTime = moment.utc('2024-11-14T08:00:00Z') // Third day devcon
     // // const mockedTime = moment.utc('2024-11-15T08:00:00Z') // Fourth day devcon
-    setCurrentTime(mockedTime)
+    // setCurrentTime(mockedTime)
 
-    return
+    // return
 
     const clear = setInterval(() => {
       setCurrentTime(moment.utc().add(7, 'hours'))
