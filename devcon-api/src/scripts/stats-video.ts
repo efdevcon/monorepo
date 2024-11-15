b0714ba4483fc0c97af1fa310f143966ed6e2200
wslyvh <wslyvh@users.noreply.github.com>
2024-11-15 15:55:04 +0700

    video stats

--- devcon-api/src/scripts/stats-video.ts
+++ devcon-api/src/scripts/stats-video.ts
@@ -11,20 +11,52 @@ async function main() {
   const { data } = await res.json()
   const sessions = data.items
 
-  const daily = sessions.filter((i: any) => dayjs(i.slot_start).isSame(dayjs(), 'day') && i.slot_room.youtubeStreamUrl_1)
-  const processedVideos = daily.filter((i: any) => !!i.sources_youtubeId)
-  const missingVideos = daily.filter((i: any) => !i.sources_youtubeId)
-
-  console.log('Missing videos')
-  for (const session of missingVideos) {
-    console.log(`- [${session.sourceId}] ${session.title}`)
-  }
-
   console.log('Total sessions (all days) #', sessions.length)
-  console.log('Dailly sessions (today) #', daily.length)
-  console.log('Processed (today)', processedVideos.length)
-  console.log('Missed (today)', missingVideos.length)
-  console.log('Daily %', Math.round((processedVideos.length / daily.length) * 100))
+  console.log()
+  const days = [12, 13, 14, 15]
+
+  for (const day of days) {
+    const daySessions = sessions.filter(
+      (i: any) =>
+        dayjs(i.slot_start).isSame(dayjs(`Nov ${day}, 2024`), 'day') &&
+        !i.doNotRecord &&
+        (day === 12
+          ? i.slot_room.youtubeStreamUrl_1
+          : day === 13
+          ? i.slot_room.youtubeStreamUrl_2
+          : day === 14
+          ? i.slot_room.youtubeStreamUrl_3
+          : i.slot_room.youtubeStreamUrl_4)
+    )
+    const processedVideos = daySessions.filter((i: any) => !!i.sources_youtubeId || !!i.sources_streamethId)
+    const missingVideos = daySessions.filter((i: any) => !i.sources_youtubeId && !i.sources_streamethId)
+    const onYoutube = daySessions.filter((i: any) => !!i.sources_youtubeId)
+    const onStreameth = daySessions.filter((i: any) => !!i.sources_streamethId)
+
+    console.log('Missing videos on Nov', day)
+    const groupedByRoom = missingVideos.reduce((acc: Record<string, any[]>, session: any) => {
+      const roomId = session.slot_roomId
+      if (!acc[roomId]) acc[roomId] = []
+      acc[roomId].push(session)
+      return acc
+    }, {})
+
+    Object.entries(groupedByRoom).forEach(([roomId, sessions]: any) => {
+      console.log(`\n${roomId}:`)
+      sessions.forEach((session: any) => {
+        console.log(`  - [${session.sourceId}] ${session.title}`)
+      })
+    })
+
+    console.log()
+    console.log(`Daily sessions (day ${day}) #`, daySessions.length)
+    console.log(`Processed (day ${day}) #`, processedVideos.length)
+    console.log(`Missed (day ${day}) #`, missingVideos.length)
+    console.log(`Daily % (day ${day}) #`, Math.round((processedVideos.length / daySessions.length) * 100))
+    console.log(`On Youtube (day ${day}) #`, onYoutube.length)
+    console.log(`On Streameth (day ${day}) #`, onStreameth.length)
+    console.log('')
+  }
 }
 
 main()
