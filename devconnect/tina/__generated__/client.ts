3ebc16825e36fa81488ca1f6e89f285b2551fc09
lassejaco <lassejacobsen@ethereum.org>
2024-12-03 12:51:58 +0700

    fix tina

--- devconnect/tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'e4b5e0e1f6a16715efd1246c3deb81a4a3490a5a', queries,  });
-export default client;
-  
