9ba9b6b421f8ce51468920715a89d469f1ff6b5e
lassejaco <lasse.jacobsen@ethereum.org>
2025-03-11 21:57:17 +0100

    icon blur fix

--- devconnect/src/common/components/SEO.tsx
+++ devconnect/src/common/components/SEO.tsx
@@ -44,7 +44,12 @@ export function SEO(props: SEOProps) {
       <meta name="twitter:description" content={description} key="tw_description" />
       <meta name="twitter:image" content={image} key="tw_image" />
 
-      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
     </Head>
   )
 }
