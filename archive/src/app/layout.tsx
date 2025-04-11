6f2e16f384fc680172ae8748668bd8b45d777082
wslyvh <wslyvh@users.noreply.github.com>
2025-04-11 09:29:30 +0200

    icons

--- archive/src/app/layout.tsx
+++ archive/src/app/layout.tsx
@@ -20,6 +20,24 @@ export const metadata: Metadata = {
   metadataBase: new URL(SITE_URL),
   description: SITE_DESCRIPTION,
   manifest: "/manifest.json",
+  icons: {
+    icon: [
+      { url: "/icons/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/icons/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      {
+        url: "/icons/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        url: "/icons/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+    apple: [{ url: "/icons/apple-touch-icon.png", type: "image/png" }],
+    shortcut: [{ url: "/favicon.ico" }],
+  },
   appleWebApp: {
     title: SITE_NAME,
     capable: true,
