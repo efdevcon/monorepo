3f691026a8e0e441b34ad34bc59d2bdc3ef0e42f
Didier Krux <didier@krux.co>
2025-05-19 18:51:14 +0200

    Add commented-out test event generation route in destino controller

--- devcon-api/src/controllers/destino.ts
+++ devcon-api/src/controllers/destino.ts
@@ -25,6 +25,41 @@ destinoRouter.get('/destino/:event', async (req: Request, res: Response) => {
   res.json(eventData)
 })
 
+// destinoRouter.get('/generate-destino-test', async (req: Request, res: Response) => {
+//   try {
+//     console.log('[generate-destino-test] Starting test event generation')
+
+//     const testEvent = {
+//       Id: '006Vj00000HYjljIAD',
+//       Name: 'ETH Cinco de Mayo',
+//       Date: {
+//         startDate: '2025-05-01',
+//       },
+//       Location: 'Interlomas, Mexico City, Mexico',
+//       Link: 'https://ethcdm.com',
+//       Twitter: '@ETHCincoDeMayo',
+//       'Type of Event': 'Other',
+//       LastModifiedDate: '2025-05-12T13:20:50.000+0000',
+//     }
+
+//     console.log('[generate-destino-test] Test event data:', testEvent)
+
+//     const testData = await destinoApi.generateDestinoEvent(testEvent, true)
+
+//     console.log('[generate-destino-test] Generated event result: OK', testData)
+
+//     if (!testData) {
+//       console.error('[generate-destino-test] No event data returned from generateDestinoEvent')
+//       return res.status(500).json({ error: 'Failed to generate event' })
+//     }
+
+//     res.json(testData)
+//   } catch (error: any) {
+//     console.error('[generate-destino-test] Error generating test event:', error)
+//     res.status(500).json({ error: 'Internal server error', details: error.message })
+//   }
+// })
+
 generateDestinoEvents()
 
 // Refresh events every hour
