589450652462f635c5dc27b46699dcd0dea8c20d
Didier Krux <didier@krux.co>
2025-06-05 19:27:43 +0100

    improve color buttons

--- devconnect/src/common/components/ticket/ColorButtonSvg.tsx
+++ devconnect/src/common/components/ticket/ColorButtonSvg.tsx
@@ -7,7 +7,13 @@ type ColorButtonSvgProps = {
 
 export const ColorButtonSvg: React.FC<ColorButtonSvgProps> = ({ color, selected }) => (
   <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40" fill="none">
-    <rect x="8.5" y="6.5" width="25" height="25" rx="0.5" stroke={selected ? 'white' : 'transparent'} />
-    <rect x="11" y="9" width="20" height="20" fill={color} />
+    {selected ? (
+      <>
+        <rect x="8.5" y="6.5" width="23" height="23" rx="0.5" stroke="white" />
+        <rect x="11" y="9" width="18" height="18" fill={color} />
+      </>
+    ) : (
+      <rect x="8" y="6" width="24" height="24" fill={color} />
+    )}
   </svg>
 )
