8e0bd7380c1e5a4c1ce9dcde06abf0d404361625
lassejaco <lasse.jacobsen@ethereum.org>
2025-08-02 07:35:08 +0200

    add back programmetab with lib import fix

--- devconnect-app/src/app/programme/ProgrammeTab.tsx
+++ devconnect-app/src/app/programme/ProgrammeTab.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-// import NewScheduleIndex from 'lib/components/event-schedule-new';
+import NewScheduleIndex from 'lib/components/event-schedule-new';
 
 interface ProgrammeTabProps {
   atprotoEvents?: any[];
@@ -10,17 +10,12 @@ export default function ProgrammeTab({
   atprotoEvents = [],
 }: ProgrammeTabProps) {
   return (
-    <div>
-      <h2>Programme Tab</h2>
-      <p>Event schedule temporarily disabled due to import issues.</p>
-      <p>Events count: {atprotoEvents.length}</p>
-    </div>
-    // <NewScheduleIndex
-    //   selectedEvent={null}
-    //   selectedDay={null}
-    //   setSelectedEvent={() => {}}
-    //   setSelectedDay={() => {}}
-    //   events={atprotoEvents}
-    // />
+    <NewScheduleIndex
+      selectedEvent={null}
+      selectedDay={null}
+      setSelectedEvent={() => {}}
+      setSelectedDay={() => {}}
+      events={atprotoEvents}
+    />
   );
 }
