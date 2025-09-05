6cb1ecb1a02161045902206902e7c94f19b84e84
lassejaco <lasse.jacobsen@ethereum.org>
2025-09-05 22:10:30 +0200

    fix mobile menu

--- devconnect-app/src/components/MobileMenu.tsx
+++ devconnect-app/src/components/MobileMenu.tsx
@@ -33,14 +33,18 @@ export default function Menu() {
   return (
     <>
       {/* Spacer for the bottom of the screen to counteract fixed position of menu */}
-      <div className="h-[72px] md:hidden pointer-events-none"></div>
+      <div
+        className="md:hidden pointer-events-none"
+        style={{ height: 'calc(72px + max(0px, env(safe-area-inset-bottom)))' }}
+      ></div>
       <nav
         className={cn(
           css['menu'],
-          'md:hidden fixed bottom-0 left-0 right-0 z-50 flex justify-center items-center border-t border-gray-200 h-[calc(72px + max(0px, env(safe-area-inset-bottom)))]'
+          'md:hidden fixed bottom-0 left-0 right-0 z-50 flex justify-center items-center border-t border-gray-200]'
         )}
         style={{
           paddingBottom: 'calc(0px + max(0px, env(safe-area-inset-bottom)))',
+          height: 'calc(72px + max(0px, env(safe-area-inset-bottom)))',
         }}
       >
         {NAV_ITEMS.map((item) => {
