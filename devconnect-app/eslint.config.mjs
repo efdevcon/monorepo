5b8e04e8a27a9b8e4910128c87457361f868b9c5
lassejaco <lasse.jacobsen@ethereum.org>
2025-10-02 21:46:08 +0200

    check

--- devconnect-app/eslint.config.mjs
+++ devconnect-app/eslint.config.mjs
@@ -10,7 +10,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals'),
+  ...compat.extends(['next/core-web-vitals']),
 ];
 
 export default eslintConfig;
