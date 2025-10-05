850189fd21aff4dfd1f83095095efbd97c407fef
lassejaco <lasse.jacobsen@ethereum.org>
2025-10-05 16:41:26 +0200

    test

--- devconnect-app/src/app/schedule/page.tsx
+++ devconnect-app/src/app/schedule/page.tsx
@@ -1,5 +1,25 @@
-import ProgrammePageContent from './page-content';
+'use client';
+import PageLayout from '@/components/PageLayout';
+import { homeTabs } from '../page-content';
+import cn from 'classnames';
+import css from './schedule.module.scss';
+import { useFavorites, useEvents } from '@/app/store.hooks';
+import { default as ScheduleLayout } from 'lib/components/event-schedule-new/layout-app';
 
-export default async function ProgrammePage() {
-  return <ProgrammePageContent />;
+export default function ProgrammePageContent() {
+  const events = useEvents();
+  const [favoriteEvents, toggleFavoriteEvent] = useFavorites();
+
+  return (
+    <PageLayout title="World's Fair" tabs={homeTabs()}>
+      <div className={cn('text-left touch-only:px-0 p-4', css['schedule-tab'])}>
+        <ScheduleLayout
+          isCommunityCalendar={false}
+          events={events}
+          favoriteEvents={favoriteEvents}
+          toggleFavoriteEvent={toggleFavoriteEvent}
+        />
+      </div>
+    </PageLayout>
+  );
 }
