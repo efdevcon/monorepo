9eb7ac4403fee2e547334c674bd5a28bd0c604d9
Didier Krux <didier@krux.co>
2025-10-24 19:44:50 +0100

    fix district layer name + add logo

--- devconnect-app/src/data/districts.ts
+++ devconnect-app/src/data/districts.ts
@@ -3,42 +3,50 @@ import type { Districts } from '@/types/api-data';
 export const districtsData: Districts = {
   "1": {
     "name": "AI",
-    "layerName": "ai",
-    "backgroundColor": "linear-gradient(0deg, rgb(170, 167, 255) 0%, rgb(246, 180, 14) 100%)"
+    "layerName": "ai-district",
+    "backgroundColor": "linear-gradient(0deg, rgb(170, 167, 255) 0%, rgb(246, 180, 14) 100%)",
+    "logo": "/images/districts/ai.svg"
   },
   "2": {
     "name": "Collectibles",
-    "layerName": "collectibles",
-    "backgroundColor": "linear-gradient(0deg, rgb(255, 133, 166) 0%, rgb(238, 221, 51) 100%)"
+    "layerName": "collectibles-district",
+    "backgroundColor": "linear-gradient(0deg, rgb(255, 133, 166) 0%, rgb(238, 221, 51) 100%)",
+    "logo": "/images/districts/collectibles.svg"
   },
   "3": {
     "name": "DeFi",
-    "layerName": "defi",
-    "backgroundColor": "linear-gradient(0deg, rgb(136, 85, 204) 0%, rgb(221, 102, 170) 100%)"
+    "layerName": "defi-district",
+    "backgroundColor": "linear-gradient(0deg, rgb(136, 85, 204) 0%, rgb(221, 102, 170) 100%)",
+    "logo": "/images/districts/defi.svg"
   },
   "4": {
     "name": "Gaming",
-    "layerName": "gaming",
-    "backgroundColor": "linear-gradient(0deg, rgb(136, 85, 204) 0%, rgb(238, 221, 51) 100%)"
+    "layerName": "gaming-district",
+    "backgroundColor": "linear-gradient(0deg, rgb(136, 85, 204) 0%, rgb(238, 221, 51) 100%)",
+    "logo": "/images/districts/gaming.svg"
   },
   "5": {
     "name": "Hardware & Wallets",
-    "layerName": "hardware-wallets",
-    "backgroundColor": "linear-gradient(0deg, rgb(116, 172, 223) 0%, rgb(238, 136, 34) 100%)"
+    "layerName": "hardware-wallets-district",
+    "backgroundColor": "linear-gradient(0deg, rgb(116, 172, 223) 0%, rgb(238, 136, 34) 100%)",
+    "logo": "/images/districts/hardware-wallets.svg"
   },
   "6": {
     "name": "L2s",
-    "layerName": "l2s",
-    "backgroundColor": "linear-gradient(0deg, rgb(68, 221, 187) 0%, rgb(116, 172, 223) 100%)"
+    "layerName": "l2s-district",
+    "backgroundColor": "linear-gradient(0deg, rgb(68, 221, 187) 0%, rgb(116, 172, 223) 100%)",
+    "logo": "/images/districts/l2s.svg"
   },
   "7": {
     "name": "Privacy",
-    "layerName": "privacy",
-    "backgroundColor": "linear-gradient(0deg, rgb(54, 54, 76) 0%, rgb(68, 221, 187) 100%)"
+    "layerName": "privacy-district",
+    "backgroundColor": "linear-gradient(0deg, rgb(54, 54, 76) 0%, rgb(68, 221, 187) 100%)",
+    "logo": "/images/districts/privacy.svg"
   },
   "8": {
     "name": "Social",
-    "layerName": "social",
-    "backgroundColor": "linear-gradient(0deg, rgb(255, 133, 166) 0%, rgb(170, 167, 255) 100%)"
+    "layerName": "social-district",
+    "backgroundColor": "linear-gradient(0deg, rgb(255, 133, 166) 0%, rgb(170, 167, 255) 100%)",
+    "logo": "/images/districts/social.svg"
   }
 };
