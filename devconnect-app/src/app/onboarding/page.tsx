e7d359c236d9d6e2305717531e9875e1db5e4f58
Didier Krux <didier@krux.co>
2025-10-25 21:49:56 +0100

    clean OnboardingPage

--- devconnect-app/src/app/onboarding/page.tsx
+++ devconnect-app/src/app/onboarding/page.tsx
@@ -1,10 +1,23 @@
-import WalletTab from './WalletTab';
-// import Auth from '@/components/Auth';
+'use client';
+
+import Onboarding from '@/components/Onboarding';
+import midjEpicCity3 from '@/images/midj-epic-city3.png';
 
 export default function OnboardingPage() {
   return (
-    // <Auth>
-    <WalletTab />
-    // </Auth>
+    <div
+      className="min-h-screen h-full flex items-center justify-center p-4 grow"
+      style={{
+        backgroundImage: `url('${midjEpicCity3.src}')`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        backgroundAttachment: 'fixed',
+      }}
+    >
+      <div className="max-w-md w-full">
+        <Onboarding />
+      </div>
+    </div>
   );
 }
