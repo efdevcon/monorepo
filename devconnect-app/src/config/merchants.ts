c490ac8292559185c8c24793b6d746f3b8d37ac3
Didier Krux <didier@krux.co>
2025-11-05 12:36:30 -0300

    add POS conf

--- devconnect-app/src/config/merchants.ts
+++ devconnect-app/src/config/merchants.ts
@@ -5,11 +5,16 @@ export interface Merchant {
 }
 
 export const MERCHANTS: Record<string, Merchant> = {
-  '6603276727aaa6386588474d': {
+  '69091751563c0c276e0ac3a9': {
     posNumber: '1',
+    id: 'pre-devcon-pop-up',
+    name: 'Pre-Devcon Pop Up',
+  },
+  '6603276727aaa6386588474d': {
+    posNumber: '2',
     id: 'cafe-cuyo',
     name: 'Cafe Cuyo',
-  }
+  },
 };
 
 export const getMerchantById = (id: string): Merchant | undefined => {
