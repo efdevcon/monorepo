b8c0442a7e0a475e356e8edb5fcaaadd2a814ed0
lassejaco <lasse.jacobsen@ethereum.org>
2025-11-05 23:32:24 +0100

    new map

--- devconnect-app/src/app/(page-layout)/map/venue-map/maps/MapWrapper.tsx
+++ devconnect-app/src/app/(page-layout)/map/venue-map/maps/MapWrapper.tsx
@@ -1,6 +1,6 @@
 import React, { forwardRef, useRef } from 'react';
 import useSVGProps from './event-listeners';
-import Map from './map-v12.svg';
+import Map from './map-14.svg';
 
 interface MapTestProps {
   onSVGElementClick: (id: string, event: React.MouseEvent<SVGElement>) => void;
