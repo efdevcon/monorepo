55efc53567b90b43f3d4e674ec5f5bc15646facf
Didier Krux <didier@krux.co>
2025-11-17 07:37:48 -0300

    update

--- devconnect-app/src/app/quests/components/InfoCard.tsx
+++ devconnect-app/src/app/quests/components/InfoCard.tsx
@@ -11,14 +11,14 @@ const InfoCard: React.FC = () => {
             className="text-sm font-bold text-[#20202b] leading-none"
             style={{ fontFamily: 'Roboto, sans-serif' }}
           >
-            Using wallets to complete Quests
+            How to complete Quests
           </p>
           <p
             className="text-xs font-medium text-[#353548] leading-[1.3]"
             style={{ fontFamily: 'Roboto, sans-serif' }}
           >
-            You can complete quests using any wallet and verify completion later
-            by connecting it to the app.
+            You can complete quests by minting POAPs using any wallet connected
+            to the app.
           </p>
         </div>
         <div className="w-8 h-8 flex-shrink-0">
