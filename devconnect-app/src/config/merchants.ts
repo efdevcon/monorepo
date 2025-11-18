96cd2e5ec8ce059426237a28db872c35077b128b
Didier Krux <didier@krux.co>
2025-11-17 22:01:10 -0300

    update merchant names

--- devconnect-app/src/config/merchants.ts
+++ devconnect-app/src/config/merchants.ts
@@ -33,22 +33,22 @@ export const MERCHANTS: Record<string, Merchant> = {
   '6911faaeea5ff64e14b83893': {
     posNumber: '4',
     id: 'burgers-1',
-    name: 'Hamburgueseria A',
+    name: 'Hamburgueseria 1A',
   },
   '6914b29b0ce6a7a6a3c725a6': {
     posNumber: '5',
     id: 'burgers-1-b',
-    name: 'Hamburgueseria B',
+    name: 'Hamburgueseria 1B',
   },
   '6911fadb0ce6a7a6a3c721d5': {
     posNumber: '6',
     id: 'burgers-2',
-    name: 'Hamburgueseria C',
+    name: 'Hamburgueseria 2A',
   },
   '6914b2b30ce6a7a6a3c725a7': {
     posNumber: '7',
     id: 'burgers-2-b',
-    name: 'Hamburgueseria D',
+    name: 'Hamburgueseria 2B',
   },
   '6911fb810ce6a7a6a3c721d8': {
     posNumber: '8',
@@ -63,12 +63,18 @@ export const MERCHANTS: Record<string, Merchant> = {
   '6911fc290ce6a7a6a3c721d9': {
     posNumber: '10',
     id: 'los-petersen',
-    name: 'Los Petersen A',
+    name: 'Los Petersen 1',
   },
   '6914a123ea5ff64e14b83d74': {
     posNumber: '11',
     id: 'los-petersen-2',
-    name: 'Los Petersen B',
+    name: 'Los Petersen 2',
+  },
+  // TODO: replace with real merchant
+  '6914a123ea5ff64e14b83d75': {
+    posNumber: '14',
+    id: 'los-petersen-3',
+    name: 'Los Petersen 3',
   },
   '6911fbc5ea5ff64e14b83896': {
     posNumber: '12',
