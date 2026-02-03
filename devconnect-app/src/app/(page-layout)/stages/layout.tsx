f515116a2d2ee1f81dbc7471ae38045862b3ab11
Didier Krux <didier@krux.co>
2026-02-03 17:11:21 +0000

    update stage tab to point to recordings

--- devconnect-app/src/app/(page-layout)/stages/layout.tsx
+++ devconnect-app/src/app/(page-layout)/stages/layout.tsx
@@ -1,28 +1,30 @@
 'use server';
-import { getProgramming, getStageEvents } from '@/utils/programming';
-import { SWRConfig } from 'swr';
+// import { getProgramming, getStageEvents } from '@/utils/programming';
+// import { SWRConfig } from 'swr';
 
 export default async function StagesDataLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const stages = await getStageEvents();
+  // API down - avoid 500 from https://devconnect.pblvrt.com/events
+  // const stages = await getStageEvents();
 
   // return children;
 
-  return (
-    <SWRConfig
-      value={{
-        fallback: {
-          'https://devconnect.pblvrt.com/events': {
-            success: true,
-            data: stages,
-          },
-        },
-      }}
-    >
-      {children}
-    </SWRConfig>
-  );
+  // return (
+  //   <SWRConfig
+  //     value={{
+  //       fallback: {
+  //         'https://devconnect.pblvrt.com/events': {
+  //           success: true,
+  //           data: stages,
+  //         },
+  //       },
+  //     }}
+  //   >
+  //     {children}
+  //   </SWRConfig>
+  // );
+  return <>{children}</>;
 }
