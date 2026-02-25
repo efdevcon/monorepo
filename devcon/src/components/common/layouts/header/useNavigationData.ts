ed7953498bae417f904e36b84c03d93150bf5a7c
Didier Krux <didier@krux.co>
2026-02-25 18:36:09 +0000

    update navigation

--- devcon/src/components/common/layouts/header/useNavigationData.ts
+++ devcon/src/components/common/layouts/header/useNavigationData.ts
@@ -322,16 +322,16 @@ const useNavigationData = () => {
       //   type: 'link',
       // },
       {
-        title: 'Devcon Archive',
+        title: 'Archive',
         url: 'https://archive.devcon.org',
         type: 'page',
         // highlight: 'archive',
       },
-      {
-        title: 'Devconnect',
-        url: 'https://devconnect.org',
-        type: 'link',
-      },
+      // {
+      //   title: 'Devconnect',
+      //   url: 'https://devconnect.org',
+      //   type: 'link',
+      // },
       // {
       //   title: 'DevAI 🦄',
       //   type: 'button',
@@ -345,6 +345,12 @@ const useNavigationData = () => {
       //   type: 'page',
       //   highlight: 'app',
       // },
+      {
+        title: 'Get Tickets',
+        url: '/tickets',
+        type: 'page',
+        // highlight: 'archive',
+      },
     ],
     footer: {
       bottom: [
